79c7113ef35176c95c2ab27ee8a86ea36a2ccfbd
Mohammad Hammoud <mohammadhammoud1921@gmail.com>
2019-03-15 14:08:41 +0200

    Update login.php

--- login.php
+++ login.php
@@ -59,6 +59,7 @@
                          <a href="index.php">Home</a>
                        </li>
                        <li><a href="ourModels.php">Available Models</a></li>
+                      <li><a href="Alexa.php">Communicate With Alexa!</a></li>
                        <li class="active"><a href="signup.php">Create An Account</a>
                        </li>
                        <li>
