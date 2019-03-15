da76c333b3af9c4b699a8334736a52d246ccdc1b
Mohammad Hammoud <mohammadhammoud1921@gmail.com>
2019-03-15 19:07:07 +0200

    Update Alexa.php

--- Alexa.php
+++ Alexa.php
@@ -137,8 +137,21 @@ Enter the 2018 Echo Dot, which is sure to continue the trend as Amazon's most po
              <img src="images/img_2.jpg" alt="Image" class="img-fluid">
            </div>
            <div class="col-md-5 col-lg-5 mr-auto order-md-1">
-             <h2 class="font-weight-bold text-uppercase display-3"><span class="d-block">Start The <div style="text-align:center;"> Replicator.</div></span> </h2>
-             <p></p>
+             <h2 class="font-weight-bold text-uppercase display-3"><span class="d-block">Start The <div style="text-align:center;"> Replicator.</div></span> </h2> <br>
+             <ul>
+               <li style="list-style-type:disc; padding:0;">To start the replicator skill, you have to call on Alexa "Alexa, open the replicator !"</li>
+               <li style="list-style-type:disc;padding:0;">In order to know what you can print, you can issue one of these commands with Alexa</li>
+                  <ul style = "margin-left: 50px">
+                    <li style="list-style-type:circle;"> "Name me a few three dimentional models you can print"</li>
+                    <li style="list-style-type:circle;"> "Name me a few three dimentional models I can print"</li>
+                    <li style="list-style-type:circle;"> "Name me a few three dimentional models"</li>
+                    <li style="list-style-type:circle;"> "Name me a few three dimentional models you have"</li>
+                    <li style="list-style-type:circle;"> "Name me a few models you have"</li>
+                    </ul>
+
+               <li style="list-style-type:disc;"></li>
+
+             </ul>
 
            </div>
 
