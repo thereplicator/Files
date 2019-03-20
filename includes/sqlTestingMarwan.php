8a59e434e799d82083a1e5c605946c347dceade6
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-20 22:44:03 +0200

    Update sqlTestingMarwan.php

--- includes/sqlTestingMarwan.php
+++ includes/sqlTestingMarwan.php
@@ -9,7 +9,7 @@
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     $tsql= "SELECT * FROM Models";
     $getResults= sqlsrv_query($conn, $tsql);
-    echo $getResults;
+    // echo $getResults;
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
