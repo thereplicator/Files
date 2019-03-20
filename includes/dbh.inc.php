72eed796fe43227445da0f1ab933dbf053cad050
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-20 21:08:18 +0200

    Added Testing File for connection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,9 +4,10 @@
 // $username = "hmh75";
 // $password=  "$$@@256NH";
 // $conn = new mysqli($host, $username,$password,$dbame);
+
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:threreplicator.database.windows.net,1433; Database = Users", "hmh75", "{$$@@256NH}");
+    $conn = new PDO("sqlsrv:server = tcp:threreplicator.database.windows.net,1433; Database = Users", "hmh75", "$$@@256NH");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -15,6 +16,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "hmh75@threreplicator", "pwd" => "{$$@@256NH}", "Database" => "Users", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "hmh75@threreplicator", "pwd" => "$$@@256NH", "Database" => "Users", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:threreplicator.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
---
+++ includes/sqlTestingMarwan.php
@@ -0,0 +1,19 @@
+<?php
+    $serverName = "tcp:threreplicator.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "Users", // update me
+        "Uid" => "hmh75", // update me
+        "PWD" => "$$@@256NH" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM Models";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
