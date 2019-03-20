33ec9e4bf06cfe6396a379dd532db3d13ff402de
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-20 20:51:19 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,7 +6,7 @@
 // $conn = new mysqli($host, $username,$password,$dbame);
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:threreplicator.database.windows.net,1433; Database = Users", "hmh75", "$$@@256NH");
+    $conn = new PDO("sqlsrv:server = tcp:threreplicator.database.windows.net,1433; Database = Users", "hmh75", "{$$@@256NH}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -15,6 +15,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "hmh75@threreplicator", "pwd" => "$$@@256NH", "Database" => "Users", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "hmh75@threreplicator", "pwd" => "{$$@@256NH}", "Database" => "Users", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:threreplicator.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
