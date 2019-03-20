14f02e4f55410a666761b313bdca40a8c5238022
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-20 23:41:31 +0200

    Update sqlTest2.php

--- includes/sqlTest2.php
+++ includes/sqlTest2.php
@@ -7,7 +7,7 @@ $db_name = 'Users';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 1433);
+mysqli_real_connect($conn, $host, $username, $password, $db_name);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
