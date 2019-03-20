4e2bafae067b21001bdfcc9ffd39dfc610beb0e0
MohamadHoudeib <mrh27>
2019-03-20 17:01:59 +0200

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,14 +2,20 @@
   echo "string";
   include_once 'dbh.inc.php';
 
-  $first = mysqli_real_escape_string($conn,$_POST["first"]);
-  $last = mysqli_real_escape_string($conn,$_POST["last"]);
-  $email = mysqli_real_escape_string($conn,$_POST["email"]);
-  $pwd = mysqli_real_escape_string($conn,$_POST["pwd"]);
+  // $first = mysqli_real_escape_string($conn,$_POST["first"]);
+  // $last = mysqli_real_escape_string($conn,$_POST["last"]);
+  // $email = mysqli_real_escape_string($conn,$_POST["email"]);
+  // $pwd = mysqli_real_escape_string($conn,$_POST["pwd"]);
 
-  $sql = "INSERT INTO users (FirstName , LastName , Email, password) VALUES ('$first','$last','$email','$pwd');";
+
+    $first = $_POST["first"];
+    $last = $_POST["last"];
+    $email = $_POST["email"];
+    $pwd = $_POST["pwd"];
+
+  $sql = "INSERT INTO dbo.users (FirstName , LastName , Email, password) VALUES ('$first','$last','$email','$pwd');";
   mysqli_query($conn, $sql);
 
-  // header("Location: ../lesson16.php");
+  header("Location: ../lesson16.php");
 
 ?>
