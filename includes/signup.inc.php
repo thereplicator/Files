4944156f290f787780ef804dc6d80fdac193f096
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-20 23:11:06 +0200

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -17,14 +17,12 @@
   $email = mysqli_real_escape_string($conn,$_POST["email"]);
   $pwd = mysqli_real_escape_string($conn,$_POST["pwd"]);
 
-
-    // $first = $_POST["first"];
-    // $last = $_POST["last"];
-    // $email = $_POST["email"];
-    // $pwd = $_POST["pwd"];
-
   // $sql = "INSERT INTO users (FirstName , LastName , Email, password) VALUES ('$first','$last','$email','$pwd');";
-  $sql = "INSERT INTO users (FirstName , LastName , Email, pwd) VALUES ('$first','$last','$email','$pwd');";
+  $sql = "INSERT INTO users (FirstName , LastName , Email, pwd)
+          VALUES ('. $first. ',
+          '. $last .',
+          '. $email.',
+          '. $pwd.');";
 
   // $sql = "INSERT INTO dbo.users (FirstName , LastName , Email, pwd) VALUES ('$first','$last','$email','$pwd');";
   mysqli_query($conn, $sql);
