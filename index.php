390f875abb6a46502ffd4f409aed8c0b846c0097
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-21 14:10:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>The Replicator</title>
+    <title>The Replicator Test</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
