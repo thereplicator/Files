9e4fba34ebbe8d756156c9271ca0648511f1ff54
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-03-21 14:09:01 +0200

    Added Images

--- index.php
+++ index.php
@@ -153,23 +153,23 @@
         <div class="row">
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="speaker text-left">
-              <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></a>
-              <h3 class="heading mb-0"><a href="#"><span>Will</span> Peters</a></h3>
-              <p>Business Consultant in Google</p>
+              <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/MarwanNour.jpeg" alt="Image" class="img-fluid"></a>
+              <h3 class="heading mb-0"><a href="#"><span>Marwan</span>Nour</a></h3>
+              <!-- <p>Business Consultant in Google</p> -->
             </div>
           </div>
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="speaker text-left">
-              <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_2.jpg" alt="Image" class="img-fluid"></a>
-              <h3 class="heading mb-0"><a href="#"><span>Abigail</span> McMillan</a></h3>
-              <p>Business Consultant in Facebook</p>
+              <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/HadiHaidar.jpeg" alt="Image" class="img-fluid"></a>
+              <h3 class="heading mb-0"><a href="#"><span>Hadi</span>Haidar</a></h3>
+              <!-- <p>Business Consultant in Facebook</p> -->
             </div>
           </div>
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="speaker text-left">
               <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_3.jpg" alt="Image" class="img-fluid"></a>
               <h3 class="heading mb-0"><a href="#"><span>Jean</span> Keenan</a></h3>
-              <p>Business Consultant in Medium</p>
+              <!-- <p>Business Consultant in Medium</p> -->
             </div>
           </div>
 
@@ -177,21 +177,21 @@
             <div class="speaker text-left">
               <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_4.jpg" alt="Image" class="img-fluid"></a>
               <h3 class="heading mb-0"><a href="#"><span>Justin</span> Clark</a></h3>
-              <p>Business Consultant in Slack</p>
+              <!-- <p>Business Consultant in Slack</p> -->
             </div>
           </div>
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="speaker text-left">
               <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_5.jpg" alt="Image" class="img-fluid"></a>
               <h3 class="heading mb-0"><a href="#"><span>Bryan</span> Forster</a></h3>
-              <p>Business Consultant in WordPress</p>
+              <!-- <p>Business Consultant in WordPress</p> -->
             </div>
           </div>
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="speaker text-left">
               <a href="speaker-single.html" class="d-block mb-3 thumbnail"><img src="images/person_6.jpg" alt="Image" class="img-fluid"></a>
               <h3 class="heading mb-0"><a href="#"><span>Matt</span> Nelson</a></h3>
-              <p>Business Consultant in Evernote</p>
+              <!-- <p>Business Consultant in Evernote</p> -->
             </div>
           </div>
 
