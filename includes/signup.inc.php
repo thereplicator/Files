d68190b251cea506e72c33bcd44a867f058c41ad
MohamadHoudeib <mrh27>
2019-03-22 16:32:45 +0200

    emptyFieldsLogin

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,17 +1,27 @@
 <?php
+if(isset($_POST['signup-submit'])){
   include_once 'dbh.inc.php';
-
   //NEED TO ESCAPE STRINGS AND HASH
   $first = $_POST["first"];
   $last = $_POST["last"];
   $email = $_POST["email"];
   $pwd = $_POST["pwd"];
+  if(empty($first) || empty($last) || empty($email) || empty($email)){
+    header("Location: ../signup.php?error=emptyFields&first=".$first."&last=".$last. "&email=".$email);
+    exit();
+  }else {
 
-  $sql = "INSERT INTO users (FirstName , LastName , Email, pwd)
-          VALUES ('$first' , '$last', '$email' , '$pwd');";
+  }
 
-  $getResults= sqlsrv_query($conn, $sql);
+  // $sql = "INSERT INTO users (FirstName , LastName , Email, pwd)
+  //         VALUES ('$first' , '$last', '$email' , '$pwd');";
+  //
+  // $getResults= sqlsrv_query($conn, $sql);
+  //
+  // header("Location: ../signup.php");
 
-  header("Location: ../signup.php");
-
+}else {
+  header("Location: ../signup.php?illegalEntry");
+  exit();
+}
 ?>
