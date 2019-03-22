bd0fea6992d4149f8ac6ac7fde453b9ecfb86618
MohamadHoudeib <mrh27>
2019-03-22 16:55:02 +0200

    SignUpErrorHandlers

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,26 +1,36 @@
 <?php
 if(isset($_POST['signup-submit'])){
-  include_once 'dbh.inc.php';
+  // include_once 'dbh.inc.php';
   //NEED TO ESCAPE STRINGS AND HASH
   $first = $_POST["first"];
   $last = $_POST["last"];
   $email = $_POST["email"];
   $pwd = $_POST["pwd"];
   if(empty($first) || empty($last) || empty($email) || empty($pwd)){
-    // header("Location: ../index.php?error=emptyFields&first=".$first."&last=".$last. "&email=".$email);
-    echo "empty";
+    header("Location: ../signup.php?error=emptyFields&first=".$first."&last=".$last. "&email=".$email);
     exit();
   }else {
-
+    if((!preg_match("/^[a-zA-Z]*$/", $first)) || !preg_match("/^[a-zA-Z]*$/", $last)){
+      header("Location: ../signup.php?error=ivalidFirstameOrLastame&first=");
+      exit();
+    }else {
+      if(strlen($pwd) < 8){
+        header("Location: ../signup.php?PasswordMusteAtLeast8Characters");
+        exit();
+      }else {
+        if(!preg_match("/^[a-zA-Z0-9!@#$%^&]*$/", $pwd)){
+          header("Location: ../signup.php?IvalidPassword");
+          exit();
+        }else {
+          $sql = "INSERT INTO users (FirstName , LastName , Email, pwd)
+                  VALUES ('$first' , '$last', '$email' , '$pwd');";
+          $getResults= sqlsrv_query($conn, $sql);
+          header("Location: ../signup.php?signupsuccess");
+          exit();
+        }
+      }
+    }
   }
-
-  // $sql = "INSERT INTO users (FirstName , LastName , Email, pwd)
-  //         VALUES ('$first' , '$last', '$email' , '$pwd');";
-  //
-  // $getResults= sqlsrv_query($conn, $sql);
-  //
-  // header("Location: ../signup.php");
-
 }else {
   header("Location: ../signup.php?illegalEntry");
   exit();
